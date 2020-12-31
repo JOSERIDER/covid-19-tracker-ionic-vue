1a2748cd93f99905ec426a9e1c103e6f1dc09246
Jose <jose.at@pm.me>
2020-12-31 10:08:45 +0100

    Added searchBar to filter countries by name.

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -13,9 +13,15 @@
           <ion-title size="large">COVID 19</ion-title>
         </ion-toolbar>
       </ion-header>
-     
+      <ion-searchbar placeholder="Country name" 
+                      inputmode="text" 
+                      type="text"
+                      autocomplete="country-name"
+                      @ionChange="onSearchChange($event)" 
+                      :debounce="250" >
+      </ion-searchbar>
       <ion-list>
-        <CountryListItem v-for="country in countries" 
+        <CountryListItem v-for="country in filterCountries" 
           :key="country.country"  :country="country" />
       </ion-list>
     </ion-content>
@@ -23,11 +29,12 @@
 </template>
 
 <script lang="ts">
-import { IonContent, IonHeader, IonList, IonPage, IonTitle, IonToolbar } from '@ionic/vue';
+// eslint-disable-line
+
+import { IonContent, IonHeader,IonSearchbar, IonList, IonPage, IonTitle, IonToolbar } from '@ionic/vue';
 import CountryListItem from '@/components/CountryListItem.vue';
 import { defineComponent } from 'vue';
 import axios from 'axios'
-
 const baseUrl = 'https://corona.lmao.ninja';
 
 export default defineComponent({
@@ -35,17 +42,41 @@ export default defineComponent({
   name: 'Home',
   data() {
     return {
-      countries : undefined
+      countries: [],
+      filterCountries : []
     }
   },
   methods: {
     getAllCountries: async function(){
       this.countries = (await axios.get(`${baseUrl}/v3/covid-19/countries`)).data
+    },
+    onSearchChange(event){
+      const countryName = String(event.detail.value).toUpperCase();
+      
+      if(countryName == "") {
+        this.filterCountries = this.countries;
+        return
+      }
+
+      if(this.countries){
+        this.filterCountries = this.countries.filter( country => {
+            const name = country['country'];
+            if(String(name).toUpperCase().startsWith(countryName) ){
+              return true;
+            }
+            return false;
+            }
+          );
+      }
     }
   },
-  mounted : function(){
+  beforeMount : function(){
     this.getAllCountries();
-    
+  },
+  watch:{
+    countries(){
+      this.filterCountries = this.countries;
+    },
   },
   components: {
     IonContent,
@@ -54,7 +85,8 @@ export default defineComponent({
     IonPage,
     IonTitle,
     IonToolbar,
-    CountryListItem
+    CountryListItem,
+    IonSearchbar
   },
 });
 </script>
